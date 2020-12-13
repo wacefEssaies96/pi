c77597e9f6bdb7ed8fd841e02cb18a62f699c38e
wacefEssaies96 <wacef.stratrait@gmail.com>
2020-12-13 13:08:07 +0100

    Added register features to android app

--- PiAndroid/app/src/main/java/com/example/webservice/MainActivity.java
+++ PiAndroid/app/src/main/java/com/example/webservice/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.webservice;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -19,30 +15,34 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String URL_DATA = "http://192.168.1.11:8088/users";
-    ArrayList<User> listuser = new ArrayList<>();
-    EditText username;
-    EditText password;
-    Button login;
+    EditText username,password;
+    Button login,register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
 
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Register.class);
+                startActivity(i);
+            }
+        });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,95 +51,30 @@ public class MainActivity extends AppCompatActivity {
                 if(user.isEmpty() || pwd.isEmpty()){
                     Toast.makeText(getApplicationContext(),"Please fill all the blanks.",Toast.LENGTH_LONG).show();
                 }else{
-
-                    //Toast.makeText(getApplicationContext(),bCryptHashString,Toast.LENGTH_LONG).show();
                     searchUser(user,pwd);
-
                 }
             }
         });
-
-        //loadlist();
-
     }
     private void searchUser(final String username, final String pass){
         final StringRequest stringRequest = new
-                StringRequest(Request.Method.GET,"http://192.168.1.11:8088/user/"+username,
+                StringRequest(Request.Method.GET,"http://192.168.1.6:8088/user/"+username,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         JSONObject object;
-                        String bCryptHashString = BCrypt.withDefaults().hashToString(12,pass.toCharArray());
                         try {
                             object = new JSONObject(response);
                             String p = object.getString("password");
-                            BCrypt.Result result = BCrypt.verifyer().verify("wacef".toCharArray(),bCryptHashString);
+                            BCrypt.Result result = BCrypt.verifyer().verify(pass.toCharArray(),p);
                             if(result.verified){
                                 Toast.makeText(getApplicationContext(),"vous etes connecté",Toast.LENGTH_LONG).show();
                             }
                             else{
                                 Toast.makeText(getApplicationContext(),"invalid",Toast.LENGTH_LONG).show();
                             }
-//                            if(object.getString("password").equals(pass)){
-//                                Toast.makeText(getApplicationContext(),"vous etes connecté",Toast.LENGTH_LONG).show();
-//                            }else{
-//                                Toast.makeText(getApplicationContext(),"invalid",Toast.LENGTH_LONG).show();
-//                            }
                         } catch (JSONException e) {
                             e.printStackTrace();
-                        }
-
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this,error.getMessage()+" error Loading Users",Toast.LENGTH_LONG).show();
-                    }
-                });
-        Volley.newRequestQueue(this).add(stringRequest);
-    }
-    private void loadlist() {
-        StringRequest stringRequest = new
-                StringRequest(Request.Method.GET,URL_DATA,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        JSONArray array = null;
-                        try {
-                            array = new JSONArray(response);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        for(int i =0;i<array.length();i++){
-                            JSONObject object = null;
-                            try {
-                                object = array.getJSONObject(i);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            int id=0;
-                            try {
-                                id = object.getInt("id");
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            String username = null;
-                            try {
-                                username = object.getString("username");
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                            String password = null;
-                            try {
-                                password = object.getString("password");
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-                            User user = new User(id,username,password);
-                            listuser.add(user);
-                            Toast.makeText(getApplicationContext(),listuser.get(0).getUsername(),Toast.LENGTH_LONG).show();
                         }
                     }
                 },
@@ -151,4 +86,5 @@ public class MainActivity extends AppCompatActivity {
                 });
         Volley.newRequestQueue(this).add(stringRequest);
     }
+
 }
